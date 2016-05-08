dcbaeeafa4e14b80743033cf107893fbc3310d5b
Jay Chetty <jaypaulchetty@gmail.com>
2016-05-08 16:02:18 +0100

    added placeholder

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 
 
 app.get('/', function (req, res) {
-    res.render('about', {restaurant: restaurant});
+    res.render('placeholder', {restaurant: restaurant});
 });
 
 app.get('/about', function (req, res) {
