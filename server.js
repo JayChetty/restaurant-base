45a88ff0ba3b518b0f81022f50380645625b0514
Jay Chetty <jaypaulchetty@gmail.com>
2016-05-08 15:18:27 +0100

    add mobile query and setup for production

--- server.js
+++ server.js
@@ -12,25 +12,19 @@ app.use(express.static('public'));
 
 
 app.get('/', function (req, res) {
-    res.render('home', {restaurant: restaurant});
+    res.render('about', {restaurant: restaurant});
 });
 
 app.get('/about', function (req, res) {
-    res.render('home', {restaurant: restaurant});
+    res.render('about', {restaurant: restaurant});
 });
 
 app.get('/menu', function (req, res) {
-    res.render('home', {restaurant: restaurant});
+    res.render('menu', {restaurant: restaurant});
 });
 
 app.get('/contact', function (req, res) {
-    res.render('home', {restaurant: restaurant});
+    res.render('contact', {restaurant: restaurant});
 });
 
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+app.listen(process.env.PORT || 3000);
